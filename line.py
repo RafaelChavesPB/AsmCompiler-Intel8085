2fa2f0db375d4ef306c1fdc12f07bfa674e494c7
rafael <rafaelchavespb@gmail.com>
2022-10-10 15:04:20 -0300

    .

---
+++ line.py
@@ -0,0 +1,14 @@
+class Line:
+    def __init__(self, **kwarg):
+        self.label = kwarg.get('label')
+        self.arg1 = kwarg.get('arg1')
+        self.arg2 = kwarg.get('arg2')
+        self.arg3 = kwarg.get('arg3')
+    
+    def __str__(self):
+        line = ""
+        line += self.label + " " if self.label else ""
+        line += self.arg1 + " " if self.arg1 else ""
+        line += self.arg2 if self.arg2 else ""
+        line += ", " + self.arg3 if self.arg3 else ""
+        return line
--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 #!/usr/bin/python3
 import sys
 import re
-from command import Command
+from line import Line
 
-pattern = [
-    r'^\s*(?P<label>\w+:)\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*,\s*(?P<arg3>\w+)\s*(?:;[\w\s\W]*)?$',
-    r'^\s*(?P<label>\w+:)\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*(?:;[\w\s\W]*)?$',
-    r'^\s*(?P<label>\w+:)\s*(?P<arg1>\w+)\s+(?:;[\w\s\W]*)?$',
-    r'^\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*,\s*(?P<arg3>\w+)\s*(?:;[\w\s\W]*)?$',
-    r'^\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*(?:;[\w\s\W]*)?$',
-    r'^\s*(?P<arg1>\w+)\s+(?:;[\w\s\W]*)?$',
-    r'^\s*(?:;[\w\s\W]*)?$'
-]
 
-filename = sys.argv[1] if len(sys.argv) > 1 else input('Asm file name: ')
-try:
-    with open(filename+'.asm', 'r') as file:
-        for index, line in enumerate(file, start=1):
-            if not line or re.match(pattern[-1], line):
-                continue
-            for p in pattern[:-1]:
-                match = re.match(p, line)
-                if match:
-                    print(Command(**match.groupdict()))
-                    break
-            else:
-                raise SyntaxError(f'Invalid syntax at line {index} -> "{line.strip()}".')
-except FileNotFoundError:
-    print(f'File "{filename}.asm" not found.')
-except Exception as err:
-    print(err)
+class Compiler:
+
+    patterns = [
+        r'^\s*(?P<label>\w+):\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*,\s*(?P<arg3>\w+)\s*(?:;[\w\s\W]*)?$',
+        r'^\s*(?P<label>\w+):\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*(?:;[\w\s\W]*)?$',
+        r'^\s*(?P<label>\w+):\s*(?P<arg1>\w+)\s+(?:;[\w\s\W]*)?$',
+        r'^\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*,\s*(?P<arg3>\w+)\s*(?:;[\w\s\W]*)?$',
+        r'^\s*(?P<arg1>\w+)\s+(?P<arg2>\w+)\s*(?:;[\w\s\W]*)?$',
+        r'^\s*(?P<arg1>\w+)\s+(?:;[\w\s\W]*)?$',
+        r'^\s*(?:;[\w\s\W]*)?$'
+    ]
+
+    registers = {'A': 0, 'B': 1, 'C': 2,
+                 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+    def __init__(self):
+        pass
+
+    def compile(self):
+        filename = sys.argv[1] if len(
+            sys.argv) > 1 else input('Asm file name: ')
+        try:
+            with open(filename+'.asm', 'r') as file:
+                for index, line in enumerate(file, start=1):
+                    if not line or re.match(self.pattern[-1], line):
+                        continue
+                    for p in self.pattern[:-1]:
+                        match = re.match(p, line)
+                        if match:
+                            print(Line(**match.groupdict()))
+                            break
+                    else:
+                        raise SyntaxError(
+                            f'Invalid syntax at line {index} -> "{line.strip()}".')
+        except FileNotFoundError:
+            print(f'File "{filename}.asm" not found.')
+        except Exception as err:
+            print(err)
