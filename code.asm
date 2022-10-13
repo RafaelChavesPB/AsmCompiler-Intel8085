10bb401f970fc18d844ca2a9490d82604aa92df1
rafael <rafaelchavespb@gmail.com>
2022-10-12 23:24:41 -0300

    Add translaters functions

--- code.asm
+++ code.asm
@@ -1,16 +1,15 @@
-label: db 10 ,10,10
-;<Program title>
 jmp start
 ;data
+data: db 0ffh, 0ffh, 0ffh
+array: ds 3
 ;code
-start:  nop
+start: org 10
 	mvi b, 3
 	mov a, b
 	adi 0
 	jpo odd
 	ani 127
 	jmp fim
-org 0
 odd: 	xri 128
 fim:	mov b,a
 hlt
